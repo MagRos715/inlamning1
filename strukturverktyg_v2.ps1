6d0f399fbf30e8d0ac4675570883fe916e06f5ea
Magnus Rosenlund <magnus.rosenlund.student@ksgyf.se>
2025-05-08 14:37:27 +0200

    Uppgift 6-7 klara, v2 klar

---
+++ strukturverktyg_v2.ps1
@@ -0,0 +1,57 @@
+# FUNKTION 1
+
+# Funktion för att skapa mappstrukturen om den inte redan existerar
+function Create-FolderIfNotExists { # Definierar funktionen och skapar ett anropsnamn
+    param ( # Deklarerar en parameter till funktionen
+        [string]$Path # [string] definierar datatypen för parametern
+    )
+    try { # Block som PS ska försöka köra
+        if (-Not (Test-Path -Path $Path)) { # Testar om sökvägen existerar
+            New-Item -ItemType Directory -Path $Path -Force | Out-Null # Ny mapp skapa om den inte redan finns
+            Write-Host "The folder '$Path' was successfully created." # Meddelar användaren att mappen skapats
+        }
+        else {
+            Write-Host "The folder '$Path' already exists." # Meddelar att mappen redan existerar
+        }
+    } catch {
+        Write-Error "Failed to create folder '$Path': $_" # Meddelar användare att ett fel uppstått $_ ersätts av felet
+    }
+}
+
+# FUNKTION 2
+
+# Funktion för att skapa logfil
+function Create-LogFile {
+    param (
+        [string]$FolderPath # [string] definierar datatypen för parametern
+    )
+    try {
+        $timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss" #Skapar variabel för tidsfomat
+        $fileName = "log-$timestamp.txt" # Skapar logfilen 
+        $fullPath = Join-Path -Path $FolderPath -ChildPath $fileName # Skapar full sökväg till logfilen
+        $logText = "Struktur skapad $timestamp" # Skapar strukturen för hur loggen ska se ut
+        Add-Content -Path $fullPath -Value $logText # Skapar en log
+        Write-Host "Log file '$fileName' was created in '$FolderPath'." # Meddelar att logfilen skapats
+    } catch {
+        Write-Error "Failed to create log file: $_" # Meddelar att logfilen inte kunde skapas
+    }
+}
+
+# PROGRAM
+
+# Program för skapa mappstrukturen och skapa log genom att kalla på funktionerna
+$name = Read-Host "What's your folder's name?" # Ber användaren att döpa sin mapp
+$basePath = Join-Path -Path (Get-Location) -ChildPath $name # Skapar en full sökväg till huvudmappen
+
+Create-FolderIfNotExists -Path $basePath # Anropar funktionen Create-FolderIfNotExists
+
+# Loop för att skapa mapparna om de inte redan finns
+$subFolders = @("logs", "scripts", "temp") # Skapar en array med de mappar som ska skapas
+foreach ($sub in $subFolders) {
+    $fullSubPath = Join-Path -Path $basePath -ChildPath $sub # Skapar full sökväg till mappen som skapas
+    Create-FolderIfNotExists -Path $fullSubPath # Skickar sökvägen till funktionen om den inte redan existerar
+}
+
+# Skapa loggfil i logs-mappen
+$logsPath = Join-Path -Path $basePath -ChildPath "logs" # Skapar sökvägen till logs
+Create-LogFile -FolderPath $logsPath # Anropar funktionen Create-logFile
